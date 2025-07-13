78de3403e0005476dc918ecb8f06b4d40cf40459
Kostya <kostantinkorocenko301@gmail.com>
2025-07-13 18:50:19 +0300

    test

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,34 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+        <article className={`TodoInfo ${todos[0].completed ? 'TodoInfo--completed' : ''}`}>
+            <h2 className="TodoInfo__title">{todos[0].title}</h2>
+
+            <a className="UserInfo" href={`mailto:${todos[0].user.email}`}>
+                {todos[0].user.name}
+            </a>
+        </article>
+
+        <article className={`TodoInfo ${todos[1].completed ? 'TodoInfo--completed' : ''}`}>
+            <h2 className="TodoInfo__title">{todos[1].title}</h2>
+
+            <a className="UserInfo" href={`mailto:${todos[1].user.email}`}>
+                {todos[1].user.name}
+            </a>
+        </article>
+
+        <article className={`TodoInfo ${todos[2].completed ? 'TodoInfo--completed' : ''}`}>
+            <h2 className="TodoInfo__title">{todos[2].title}</h2>
+
+            <a className="UserInfo" href={`mailto:${todos[2].user.email}`}>
+                {todos[2].user.name}
+            </a>
+        </article>
+
+
+        <article className={`TodoInfo ${todos[3].completed ? 'TodoInfo--completed' : ''}`}>
+            <h2 className="TodoInfo__title">{todos[3].title}</h2>
+
+            
+
+        </article>
+    </section>)
